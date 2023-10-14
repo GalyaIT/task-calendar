8bbbbaffb55e9b963f48cffffeb47b7520e0f5af
Galina Ivanova <52123474+GalyaIT@users.noreply.github.com>
2023-10-14 16:28:11 +0300

    check for logged in user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import React, {useEffect} from 'react'
+import {Routes, Route, useNavigate} from 'react-router-dom'
+import './App.css'
 import Home from './containers/Home/Home'
 import Login from './components/Login/Login'
-import './App.css'
+import { fetchUser } from './utils/FetchUser';
 
 
 function App() {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+   const User = fetchUser();
+   if (!User) navigate('/login');
+ }, []);
+
   return (
     <div className='todo__app'>
       <Routes>
